2353e37c433309d9df30e023582926dc4f91023f
phueb <info@philhuebner.com>
2019-11-12 16:41:54 -0600

    there are 6 levels

--- make_corpus.py
+++ make_corpus.py
@@ -4,6 +4,6 @@ from newsela.corpus import Corpus
 
 ARTICLES_DIR = '/home/ph/newsela_article_corpus_2016-01-29/articles'
 
-for level in range(5):
+for level in range(6):
     c = Corpus.from_level(level, ARTICLES_DIR)
     print(f'Found {c.num_words:>9,} for level={c.level}')
